415eee2248e0538b7a42887c23a2d2f68c5ed827
ANC <andersostergaard.christiansen@nov.com>
2021-03-28 11:12:24 +0200

    open doc in demo,

--- demo.bat
+++ demo.bat
@@ -1,10 +1,14 @@
+:: Open api documentation page
+start api_documentation.html
+
 :: Build docker image and start application inside a docker container
 start docker-compose up
 
 :: Make curl request to the api of the application; continue when application is running
 :repeat
     (curl localhost:8000/clear_blacklist | find "true") || goto :repeat
-	
+
+:: Run all tests
 python -m unittest tests.py
 pause
 
